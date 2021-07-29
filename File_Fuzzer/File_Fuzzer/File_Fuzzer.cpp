91228fdade76d6a37238b3c2732486d55dbf16c9
Zohdy <abdelaziz.zohdy@gmail.com>
2021-07-29 16:45:13 +0200

    handling command line argument

--- File_Fuzzer/File_Fuzzer/File_Fuzzer.cpp
+++ File_Fuzzer/File_Fuzzer/File_Fuzzer.cpp
@@ -1,7 +1,22 @@
 #include "Fuzzer.h"
 
 void ParseArgument(int argc, char* argv[]) {
-
+	
+	CHAR CommandLineArgument[2][10] = { "-x","-e" };
+	int index = 0;
+	for (int i = 0; i < argc; i++) {
+		if (strcmp(argv[i], CommandLineArgument[index]) == 0)
+		{
+			if (index == 0) {
+				ExePath = argv[i + 1];
+				index++;
+			}
+			else if (index == 1) {
+				FileExtension = argv[i + 1];
+				index++;
+			}
+		}
+	}
     return;
 }
 
